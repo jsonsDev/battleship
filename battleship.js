43d40a4c725c245e1f801e307ab0282566ab9ee6
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-21 14:04:27 +0700

    Write pseudocode for 1D version.

---
+++ battleship.js
@@ -0,0 +1,37 @@
+// Declare three variables to hold locations of each cell of the ship
+// location1, location2, location3
+
+// Declare a variable to hold the user's current guess
+// guess
+
+// Declare a variable to hold the number of hits
+// hits = 0
+
+// Declare a variable to hold the number of guesses
+// guesses = 0
+
+// Declare a variable to keep track of whether ship is sunk or not
+// isSunk = false
+
+// Loop: while the ship is not sunk
+    // Get the user's guess
+    
+    // Compare the user's input to valid input values
+
+    // If the user's guess is invalid
+        // Tell user to enter a valid number
+    // Else
+        // Add one to guesses
+
+        // If the user's guess matches a location
+            // Add one to the number of hits
+
+            // If number of hits is 3
+                // Set isSunk to true
+                // Tell user "You sank my battleship!"
+            // End if
+        // End if
+    // End else
+// End loop
+
+// Tell user stats
