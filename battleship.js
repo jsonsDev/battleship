12b342fbc512ec315543c197fdefc6f64a7db2a7
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-11-23 16:56:27 -0600

    Add model object code.

--- battleship.js
+++ battleship.js
@@ -12,3 +12,27 @@ let view = {
         cell.setAttribute("class", "miss");
     }
 };
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    
+    fire(guess) {
+
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
+        }
+        return false;
+    }
+};
