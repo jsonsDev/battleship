813f3588a02ddd5f0f20a8a60826d05046883774
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-11-25 16:15:22 -0600

    Write controller object and parseGuess function.

--- battleship.js
+++ battleship.js
@@ -29,7 +29,7 @@ let model = {
                 ship.hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("HIT!");
-                if (this.shipsSunk(ship)) {
+                if (this.isSunk(ship)) {
                     view.displayMessage("You sank my battleship!");
                     this.shipsSunk++;
                 }
@@ -44,3 +44,39 @@ let model = {
         return (!ship.hits.includes(""));
     }
 };
+
+let controller = {
+    guesses: 0,
+
+    processGuess(guess) {
+
+    }
+};
+
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        let firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize ||
+            column < 0 || column >= model.boardSize) {
+                alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
